3164b32e0bb8c2a950e4afe2882adacfa5a4ab91
Tim <timsn@posteo.de>
2015-10-19 14:42:47 +0200

    api erweiterung

--- rest-api.js
+++ rest-api.js
@@ -1,3 +1,105 @@
+/dice:
+  get:
+    description: Gives you a single dice roll
+    responses:
+      200:
+        body:
+          application/json:
+            schema: | 
+              { "type": "object",
+                "properties": {   
+                  "number": {"type": "int"},
+                  "required": true
+                }, 
+              }
+            example: '{ "number": 4 }'
+
+/games:
+  get:
+    description: Meta information about running games of restopoly
+    responses:
+      200:
+        body:
+          application/json:
+            schema: | 
+              { "type": "object",
+                "properties": {
+                  "id": {"type": "int"},
+                  "players": list_of_players,
+                  "round": {"type": "int"},
+                },
+                "required": [ "id" ]
+              }
+
+/boards/{gameid}:
+  get:
+    description: Information about the boards and locations
+    responses:
+      200:
+        body:
+          application/json:
+            schema: | 
+              { "type": "object",
+                "properties": {
+                  "id": {"type": "int"},
+                  "fields": list_of_fields,
+                },
+                "required": [ "id" ]
+              }
+  /{playerId}:
+    get:
+      responses:
+        200:
+          body:
+            application/json:
+              schema: |
+                { "type": "object",
+                  "properties": {
+                    "id": {"type": "int"},
+                    "position": {"type": "int"},
+                  }
+                }
+
+/banks/{gameid}:
+  get:
+    description: Money management
+    responses:
+      200:
+        body:
+          application/json:
+            schema |
+              { "type": "object",
+                "properties": {
+                  "balance": {"type": "number"},
+                }
+              }
+
+/decks/{gameid}:
+  /chance:
+    get: 
+      description: Gets a chance card
+      responses:
+        200:
+          body:
+            application/json:
+              schema: card
+              example: |
+                { "name": "Go to Jail",
+                  "text": "Go to jail, do not travel across 'go' and don't receive $200 "
+                }
+  /community
+    get: 
+      description: Gets a community chest card
+      responses:
+        200:
+          body:
+            application/json:
+              schema: card
+              example: |
+                { "name": "Go to Jail",
+                  "text": "Go to jail, do not travel across 'go' and don't receive $200 "
+                }
+
 /player
   get:
     description: Liefert alle spieler Informationen
