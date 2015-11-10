c4589945aa5cd20b9500e738d18f93b7dbc08b00
Alexander Holland <alexander.holland@live.de>
2015-11-11 00:59:44 +0100

    dispatch http client hinzugefuegt

--- src/main/scala/JettyMain.scala
+++ src/main/scala/JettyMain.scala
@@ -39,5 +39,6 @@ object JettyMain {
     server setHandler webApp
 
     server.start()
+
   }
 }
--- src/main/scala/ScalatraBootstrap.scala
+++ src/main/scala/ScalatraBootstrap.scala
@@ -1,9 +1,12 @@
+import de.vs.http.client.MonopolyRESTClient
 import de.vs.rest.server.monopoly.app._
 import org.scalatra._
 import javax.servlet.ServletContext
 
 class ScalatraBootstrap extends LifeCycle {
+
   override def init(context: ServletContext) {
     context.mount(new MonopolyServlet, "/*")
+    MonopolyRESTClient
   }
 }
